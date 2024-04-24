fff7d4d53572a76d3454c8a2d3e437213d288b32
Atroci <hugomoreiradecarvalho@gmail.com>
2024-04-25 00:02:16 +0100

    new test

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -2,12 +2,11 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
-      apiToken: "xpQF5xophPSWAamedT6aWHWd",
-      appFilter: "apointoo-backend",
-      teamFilter: "hugo-moreira-de-carvalhos-projects",
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+      teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
+      roles: ["strapi-super-admin"],
     },
   },
 });

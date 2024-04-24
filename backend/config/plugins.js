618284c5cea13814319f0f9ef047ce0e350a5bf0
Atroci <hugomoreiradecarvalho@gmail.com>
2024-04-24 23:51:34 +0100

    more fix

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+      apiToken: "xpQF5xophPSWAamedT6aWHWd",
+      appFilter: "apointoo-backend",
+      teamFilter: "hugo-moreira-de-carvalhos-projects",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
+});
